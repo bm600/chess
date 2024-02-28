1b2d27e0f036a6210528384ea60663eac3476c39
bm600 <blakemcgee60@gmail.com>
2024-02-28 14:01:45 -0700

    Implemented models

---
+++ shared/src/main/java/model/GameData.java
@@ -0,0 +1,66 @@
+package model;
+
+import chess.ChessGame;
+
+import java.util.Objects;
+
+public class GameData {
+    int gameID;
+    String whiteUsername;
+    String blackUsername;
+    String gameName;
+    ChessGame game;
+
+    public GameData(int ID, String wUsername, String bUsername, String gName, ChessGame game){
+        this.gameID = ID;
+        this.whiteUsername = wUsername;
+        this.blackUsername = bUsername;
+        this.gameName = gName;
+        this.game = game;
+    }
+
+    public int getGameID(){
+        return gameID;
+    }
+
+    public String getWhiteUsername(){
+        return whiteUsername;
+    }
+
+    public String getBlackUsername(){
+        return blackUsername;
+    }
+
+    public String getGameName(){
+        return gameName;
+    }
+
+    public ChessGame getGame() {
+        return game;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameData gameData = (GameData) o;
+        return gameID == gameData.gameID && Objects.equals(whiteUsername, gameData.whiteUsername) && Objects.equals(blackUsername, gameData.blackUsername) && Objects.equals(gameName, gameData.gameName) && Objects.equals(game, gameData.game);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(gameID, whiteUsername, blackUsername, gameName, game);
+    }
+
+    @Override
+    public String toString() {
+        return "GameData{" +
+                "gameID=" + gameID +
+                ", whiteUsername='" + whiteUsername + '\'' +
+                ", blackUsername='" + blackUsername + '\'' +
+                ", gameName='" + gameName + '\'' +
+                ", game=" + game +
+                '}';
+    }
+
+}
---
+++ shared/src/main/java/model/UserData.java
@@ -0,0 +1,47 @@
+package model;
+
+import java.util.Objects;
+
+public class UserData {
+    String username;
+    String password;
+    String email;
+
+    public UserData(String username, String password, String email){
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername(){
+        return this.username;
+    }
+    public String getPassword(){
+        return this.password;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+
+    @Override
+    public String toString() {
+        return "UserData{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserData userData = (UserData) o;
+        return Objects.equals(username, userData.username) && Objects.equals(password, userData.password) && Objects.equals(email, userData.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password, email);
+    }
+}
